4bc891097a803ce76db49cf7c558e84bf795fbf4
warxx <ivan@nexpur.com>
2021-05-29 14:21:41 +0300

    'create-tasks-and-status-tables-with-migrations'

---
+++ database/migrations/2021_05_29_111142_add_status_id_to_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusIdToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unsignedBigInteger('status_id');
+            $table->foreign('status_id')
+                        ->references('id')->on('status')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}
